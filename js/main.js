8f7fb5a8d07a38008ec874293eda01570f67e51e
Rosa-Sot12 <18390846@uagro.mx>
2025-03-18 19:28:59 -0600

    Subiendo ejercicio de arreglos

---
+++ js/main.js
@@ -0,0 +1,8 @@
+let nuevoArreglo = (numero) => {
+    let arreglo = []
+    for (let num = 1; num <= numero; num++ )
+        arreglo.push(num) 
+        return arreglo;
+        
+}
+console.log(nuevoArreglo(5))
